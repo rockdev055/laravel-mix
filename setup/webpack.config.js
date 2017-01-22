700bc6ee10a0df9ebad79099ec2847a09a791f4e
rockdev55 <steve264225@gmail.com>
2017-01-22 01:19:25 -0500

    Fix config merging - closes #121

--- setup/webpack.config.js
+++ setup/webpack.config.js
@@ -234,7 +234,7 @@ module.exports.devServer = {
  */
 
 module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.ProvidePlugin(Mix.js.autoload || {
+    new webpack.ProvidePlugin({
         jQuery: 'jquery',
         $: 'jquery',
         jquery: 'jquery'
