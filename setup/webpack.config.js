89abc02fcfecdb6df14956554862d1eeb6a3fe90
rockdev55 <steve264225@gmail.com>
2017-01-23 23:43:24 -0500

    Angular looks for window.jQuery

--- setup/webpack.config.js
+++ setup/webpack.config.js
@@ -235,10 +235,11 @@ module.exports.devServer = {
  */
 
 module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.ProvidePlugin({
+    new webpack.ProvidePlugin(Mix.js.autoload || {
         jQuery: 'jquery',
         $: 'jquery',
-        jquery: 'jquery'
+        jquery: 'jquery',
+        'window.jQuery': 'jquery'
     }),
 
     new plugins.FriendlyErrorsWebpackPlugin(),
