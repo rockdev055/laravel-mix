e56a6875b52b907d9389478a348a8f61a4c1c319
rockdev55 <steve264225@gmail.com>
2017-01-27 15:26:43 -0500

    Move duplicate assertions below

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let path = require('path');
+let assert = require('assert');
 let Mix = require('./Mix');
 
 /**
@@ -8,6 +9,9 @@ let Mix = require('./Mix');
  * @param {string} output
  */
 module.exports.js = (entry, output) => {
+    assert(entry && (typeof entry === 'string' || Array.isArray(entry)), 'Missing required parameter 1: entry');
+    assert(output && typeof output === 'string', 'Missing required parameter 2: output');
+
     entry = [].concat(entry).map(file => {
         return new Mix.File(path.posix.resolve(file)).parsePath();
     });
@@ -92,6 +96,9 @@ module.exports.less = (src, output) => {
  * @param {string} output
  */
 module.exports.preprocess = (type, src, output) => {
+    assert(src && typeof src === 'string', 'Missing required parameter 1: src');
+    assert(output && typeof output === 'string', 'Missing required parameter 2: output');
+
     src = new Mix.File(path.posix.resolve(src)).parsePath();
     output = new Mix.File(output).parsePath();
 
